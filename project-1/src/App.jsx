9bd608bba6c599153cc328811ebe6be64ce804df
rounakdey6 <98476572+rounakdey6@users.noreply.github.com>
2024-09-08 20:45:06 +0530

    first commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Shoe from './components/Shoe';
+const App =() =>{
+  return <div>
+   <Navigation/>
+   <Shoe/>
+  </div>
+}
+
+export default App
